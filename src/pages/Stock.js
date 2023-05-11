ce99b5cfe29d3b2a5ec27c2c4dce85c57258eac5
connorb023 <connorcb@yahoo.com>
2023-05-11 12:35:22 -0600

    stock page

---
+++ src/pages/Stock.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Stock = (props) => {
+  const stock = props.stockData.stocks.filter((stock) => {
+    //if the stock symbol matches the parameter return true
+    return (stock.symbol === props.match.params.symbol)
+  })[0]
+  return (
+    <div>
+      <h3>Name: {stock.name}</h3>
+      <h3 className="price">Price: {stock.lastPrice}</h3>
+    </div>
+  )
+}
+
+export default Stock;
